c919e79e6254b1f7f5c6e294e474c0ffb4902075
Zliced13 <07e5297d5b@c0x0.com>
2023-11-30 22:51:30 -0700

    :recycle: Removed `SplashPage` project and archived it.
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

--- SplashPage/AppShell.xaml.cs
+++
@@ -1,6 +0,0 @@
-namespace SplashPage {
-    // ReSharper disable once RedundantExtendsListEntry
-    public partial class AppShell : Shell {
-        public AppShell() { InitializeComponent(); }
-    }
-}
