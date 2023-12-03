6c36a7b17859fe48f9819166447f66c69eee5c06
Zliced13 <07e5297d5b@c0x0.com>
2023-12-02 17:01:44 -0700

    Simplified method `CreateMauiApp` in the source file, `FinalProject/MauiProgram.cs`
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

--- FinalProject/MauiProgram.cs
+++ FinalProject/MauiProgram.cs
@@ -1,15 +1,10 @@
 namespace FinalProject {
     public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
+        public static MauiApp CreateMauiApp()
+            => MauiApp.CreateBuilder().UseMauiApp<App>()
                 .ConfigureFonts(fonts => {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-            return builder.Build();
-        }
+                }).Build();
     }
 }
