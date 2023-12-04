d77ea52c529ed40130e3c22a153217ec6d511502
Zliced13 <07e5297d5b@c0x0.com>
2023-12-03 23:50:57 -0700

    :recycle: Add SQLite support
    This commit adds SQLite support to the project:
    
    - Adds GymManDB and GymMember classes for database access/entities
    - Uses SQLiteAsyncConnection from sqlite-net-pcl package
    - Updates Paket to use sqlite-net-pcl (supports .NET Standard)
    
    This provides a starting point for:
    
    - Connecting to an SQLite database
    - Storing/retrieving GymMember entities
    - Authenticating users
    
    Additional work would flesh out functionality, but gets core data access in place.
    
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

---
+++ FinalProject/GymManDB.cs
@@ -0,0 +1,46 @@
+using SQLite;
+// using Microsoft.Maui.Essentials;
+
+namespace FinalProject {
+    // ReSharper disable once InconsistentNaming
+    /// <summary>
+    /// A class for editing and creating
+    /// databases related to Gym Management.
+    /// 'GymManDB' is just short
+    /// for 'GymManagementDatabase'.
+    /// </summary>
+    public class GymManDB {
+        /// <summary>
+        /// A global private readonly variable for use
+        /// within the <see cref="GymManDB"/> class.
+        /// </summary>
+        private readonly SQLiteAsyncConnection database;
+
+        /// <summary>
+        /// A constructor for initializing a new
+        /// instance of the <see cref="GymManDB"/> class.
+        /// </summary>
+        /// <param name="dbPath">
+        /// The path to the database file (*.db/*.sqlite),
+        /// can be an absolute or relative path.
+        /// </param>
+        public GymManDB(string dbPath) {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<GymMember>().Wait();
+        }
+
+        /// <summary>
+        /// Gets a user from the database based on
+        /// their email and password.
+        /// </summary>
+        /// <param name="email">The user's email.</param>
+        /// <param name="password">The user's password.</param>
+        /// <returns>
+        /// A <see cref="GymMember"/>
+        /// <see cref="Task{TResult}"/> object.
+        /// </returns>
+        public async Task<GymMember> GetUserAsync(string email, string password)
+            => await database.Table<GymMember>().Where(m => m.Email.Equals(email)
+                && m.Password.Equals(password)).FirstOrDefaultAsync();
+    }
+}
---
+++ FinalProject/GymMember.cs
@@ -0,0 +1,20 @@
+using SQLite;
+
+namespace FinalProject {
+    /// <summary>Represents a member of a gym.</summary>
+    public class GymMember {
+        // ReSharper disable once InconsistentNaming
+        /// <summary>The member's ID</summary>
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        /// <summary>The member's username.</summary>
+        public string Username { get; set; }
+
+        /// <summary>The member's email.</summary>
+        public string Email { get; set; }
+
+        /// <summary>The member's password.</summary>
+        public string Password { get; set; }
+    }
+}
