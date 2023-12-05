94105ce4ddafbaadb74cd5a56920fedc3c2e89da
Zliced13 <07e5297d5b@c0x0.com>
2023-12-05 16:46:47 -0700

    🔨 Refactor XAML code and add sign-up functionality
    Refactor the XAML code in multiple files to improve code readability and maintainability. Additionally, add sign-up functionality to the SignUp.xaml file.
    
    - Modify App.xaml.cs:
      - Add XML documentation comments to the class and constructor.
      - Remove the redundant "ReSharper disable" comment.
    
    - Modify AppShell.xaml.cs:
      - Add XML documentation comments to the class and constructor.
      - Remove the redundant "ReSharper disable" comment.
    
    - Modify MainPage.xaml.cs:
      - Add XML documentation comments to the class and constructor.
      - Add XML documentation comments to the OnClickLogIn method.
    
    - Modify MauiProgram.cs:
      - Add XML documentation comments to the class and CreateMauiApp method.
    
    - Modify App.xaml.cs (Windows):
      - Remove the unused using statement.
      - Add XML documentation comments to the class and CreateMauiApp method.
      - Remove redundant comments.
    
    - Modify SignUp.xaml:
      - Uncomment the username input section and add appropriate comments.
      - Modify the Sign-Up button text to "Sign-Up" and update the Clicked event handler to "OnClickedSignUp".
    
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

--- FinalProject/App.xaml.cs
+++ FinalProject/App.xaml.cs
@@ -1,9 +1,14 @@
 namespace FinalProject {
+    // ReSharper disable once RedundantExtendsListEntry
+    /// <summary>
+    /// A class that adds functionality to the
+    /// relevant XAML content page.
+    /// </summary>
     public partial class App : Application {
-        public App() {
-            InitializeComponent();
-
-            MainPage = new AppShell();
-        }
+        /// <summary>
+        ///  A constructor for initializing a new
+        ///  instance of the <see cref="App"/> class.
+        /// </summary>
+        public App() { InitializeComponent(); MainPage = new AppShell(); }
     }
 }
--- FinalProject/AppShell.xaml.cs
+++ FinalProject/AppShell.xaml.cs
@@ -1,5 +1,14 @@
 namespace FinalProject {
+    // ReSharper disable once RedundantExtendsListEntry
+    /// <summary>
+    /// A class that adds functionality to the
+    /// relevant XAML content page.
+    /// </summary>
     public partial class AppShell : Shell {
-        public AppShell() { InitializeComponent(); }
+        /// <summary>
+        /// A constructor for initializing a new
+        /// instance of the <see cref="AppShell"/> class.
+        /// </summary>
+        public AppShell() => InitializeComponent();
     }
 }
