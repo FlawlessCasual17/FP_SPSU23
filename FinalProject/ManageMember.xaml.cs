a27b4a01832b5988c3b506f60ef40be485f619fc
Zliced13 <07e5297d5b@c0x0.com>
2023-12-09 21:49:20 -0700

    :tada: Add manage member page
    This commit adds a new ManageMember page:
    
    - Creates ManageMember.xaml page
    - Adds basic label content
    - Adds ManageMember.xaml.cs code behind
    - Registers ManageMember page in project file
    
    No functionality added yet, but this lays the foundation for a page to view and manage gym member details in the future.
    
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

---
+++ FinalProject/ManageMember.xaml.cs
@@ -0,0 +1,6 @@
+namespace FinalProject {
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class ManageMember : ContentPage {
+        public ManageMember() => InitializeComponent();
+    }
+}
