80901c1348185f8cec4d87042cf1b683b53b865d
Zliced13 <07e5297d5b@c0x0.com>
2023-12-09 22:35:36 -0700

    :pencil: Add nullable annotations
    This commit adds nullable annotations to properties:
    
    - Sets GymMember properties to nullable where appropriate
    - Updates assembly and file version formats
    
    No functional changes, this is to support null safety features in .NET:
    
    - Prevent potential null reference errors
    - Improve type safety
    
    Signed-off-by: Zliced13 <07e5297d5b@c0x0.com>

--- FinalProject/GymMember.cs
+++ FinalProject/GymMember.cs
@@ -3,34 +3,34 @@ namespace FinalProject {
     public class GymMember {
         // ReSharper disable once InconsistentNaming
         /// <summary>The member's ID</summary>
-        public string ID { get; init; }
+        public string? ID { get; init; }
 
         /// <summary>The member's username.</summary>
-        public string Username { get; init; }
+        public string? Username { get; init; }
 
         /// <summary>The member's password.</summary>
-        public string Password { get; init; }
+        public string? Password { get; init; }
 
-        public string FirstName { get; init; }
+        public string? FirstName { get; init; }
 
-        public string LastName { get; init; }
+        public string? LastName { get; init; }
 
-        public string Address { get; init; }
+        public string? Address { get; init; }
 
-        public string City { get; init; }
+        public string? City { get; init; }
 
-        public string Region { get; init; }
+        public string? Region { get; init; }
 
-        public string PostalCode { get; init; }
+        public string? PostalCode { get; init; }
 
-        public string Country { get; init; }
+        public string? Country { get; init; }
 
-        public string PhoneNumber { get; init; }
+        public string? PhoneNumber { get; init; }
 
         /// <summary>The member's email.</summary>
-        public string Email { get; init; }
+        public string? Email { get; init; }
 
-        public string Gender { get; init; }
+        public string? Gender { get; init; }
 
         public DateTime DateOfBirth { get; init; }
 
